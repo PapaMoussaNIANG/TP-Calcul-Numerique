0608359cb11eb844f4909b83b969cfb8568319f4
shamsTarbiz <pispaniang1@gmail.com>
2025-12-10 14:03:03 +0100

    set RHS, Grid point and implementation of relative forward error

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -49,20 +49,37 @@ void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *k
 
 void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1){
   // TODO: Compute RHS vector
+  RHS[0] = *BC0;
+  for(int i = 1; i < *la - 1; ++i){
+    RHS[i] = 0;
+  }
+  RHS[*la - 1] = *BC1;
 }  
 
 void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1){
   // TODO: Compute the exact analytical solution at each grid point
   // This depends on the source term f(x) used in set_dense_RHS_DBC_1D
+  for(int i = 0; i < *la; ++i){
+    EX_SOL[i] = *BC0 + X[i] * (*BC1 - *BC0);
+  }
 }  
 
 void set_grid_points_1D(double* x, int* la){
   // TODO: Generate uniformly spaced grid points in [0,1]
+  double h = 1.0f / ((double)*la + 1.0f);
+  for(int i = 0; i < *la; ++i){
+    x[i] = h + (double) i * h;
+  }
 }
 
 double relative_forward_error(double* x, double* y, int* la){
   // TODO: Compute the relative error using BLAS functions (dnrm2, daxpy or manual loop)
-  return 0.0;
+  double norm_denominator = cblas_dnrm2(*la, x, 1);
+
+  cblas_daxpy(*la, -1, x, 1, y, 1);
+  double norm_numerator = cblas_dnrm2(*la, y, 1);
+
+  return norm_numerator / norm_denominator;
 }
 
 int indexABCol(int i, int j, int *lab){
